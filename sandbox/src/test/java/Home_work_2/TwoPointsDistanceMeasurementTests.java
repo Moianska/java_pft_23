567d88919d5482e111d9fe01fd101deda026875e
Moianska <moyanskaya@gmail.com>
2021-02-27 19:11:47 +0200

    Test for Distance

---
+++ sandbox/src/test/java/Home_work_2/TwoPointsDistanceMeasurementTests.java
@@ -0,0 +1,14 @@
+package Home_work_2;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TwoPointsDistanceMeasurementTests {
+
+    @Test
+    public void testDistance() {
+        Point p1 = new Point(1,1);
+        Point p2 = new Point(4,5);
+        Assert.assertEquals(p1.distance(p2), 5);
+    }
+}
