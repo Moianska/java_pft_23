e9058ba9c32cc127981363eb8b7d1ae0e0ebdc90
Moianska <moyanskaya@gmail.com>
2021-03-07 21:41:55 +0200

    SessionHelper changes

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -11,6 +11,7 @@ public class ApplicationManager {
 
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
+    private SessionHelper sessionhelper;
 
     public void init() {
         wd = new FirefoxDriver();
@@ -18,19 +19,8 @@ public class ApplicationManager {
         wd.get("http://localhost/addressbook/");
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
-        login();
-    }
-
-    public void login() {
-      login("admin", "secret");
-    }
-
-    public void login(String userName, String userPassword) {
-      wd.findElement(By.name("user")).clear();
-      wd.findElement(By.name("user")).sendKeys(userName);
-      wd.findElement(By.name("pass")).clear();
-      wd.findElement(By.name("pass")).sendKeys(userPassword);
-      wd.findElement(By.xpath("//input[@value='Login']")).click();
+        sessionhelper = new SessionHelper(wd);
+        sessionhelper.login("admin", "secret");
     }
 
     public void stop() {
