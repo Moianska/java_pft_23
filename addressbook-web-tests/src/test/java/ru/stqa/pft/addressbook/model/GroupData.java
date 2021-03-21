78c79e5da6b21eaeabf46531158b03fccaa1a9ff
Moianska <moyanskaya@gmail.com>
2021-03-21 16:31:49 +0200

    Add check for Group Modification

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -3,18 +3,31 @@ package ru.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
+    private final String id;
     private final String groupName;
     private final String groupHeader;
     private final String groupFooter;
+
+    public GroupData(String groupName, String groupHeader, String groupFooter, String id) {
+        this.groupName = groupName;
+        this.groupHeader = groupHeader;
+        this.groupFooter = groupFooter;
+        this.id = id;
+    }
 
     public GroupData(String groupName, String groupHeader, String groupFooter) {
         this.groupName = groupName;
         this.groupHeader = groupHeader;
         this.groupFooter = groupFooter;
+        this.id = null;
     }
 
     public String getGroupName() {
         return groupName;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getGroupHeader() {
@@ -28,7 +41,8 @@ public class GroupData {
     @Override
     public String toString() {
         return "GroupData{" +
-                "groupName='" + groupName + '\'' +
+                "id='" + id + '\'' +
+                ", groupName='" + groupName + '\'' +
                 '}';
     }
 
@@ -37,11 +51,11 @@ public class GroupData {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GroupData groupData = (GroupData) o;
-        return Objects.equals(groupName, groupData.groupName);
+        return Objects.equals(id, groupData.id) && Objects.equals(groupName, groupData.groupName);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(groupName);
+        return Objects.hash(id, groupName);
     }
 }
