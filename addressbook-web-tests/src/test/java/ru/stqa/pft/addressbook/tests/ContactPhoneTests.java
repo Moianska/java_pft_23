73c49d19a64c685a329b5db7b795433c160831cc
Moianska <moyanskaya@gmail.com>
2021-04-14 00:38:36 +0300

    Modify ContactPhoneTests with DB-loading data

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -11,17 +12,19 @@ import static org.hamcrest.MatcherAssert.*;
 
 public class ContactPhoneTests extends TestBase{
 
-    @Test
-    public void testContactPhones() {
-
-        String groupName = app.group().defineGroupName();
-        app.goTo().backHome();
-
-        if (app.contact().all().size() == 0){
+    @BeforeMethod
+    public void ensurePreconditions() {
+        if (app.db().contacts().size() == 0) {
+            String groupName = app.group().defineGroupName();
+            app.goTo().backHome();
             app.contact().create(new ContactData().withName ("Mike").withLastName("Jordan")
                     .withMobilePhone("+33111222333").withEmail("terry.p@google.com").withHomeAddress("USA, Montana")
-                    .withGroup(groupName).withWorkPhone("121 5545").withHomePhone("444 5555"));
+                    .withGroup(groupName));
         }
+    }
+
+    @Test
+    public void testContactPhones() {
 
         ContactData contact = app.contact().all().iterator().next();
         ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
