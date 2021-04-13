99cfea4ed08ef0559557b546855d6051353b0c80
Moianska <moyanskaya@gmail.com>
2021-04-14 00:35:23 +0300

    Modify ContactEmailTests with DB-loading data

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactHomeAddressTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactHomeAddressTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -8,20 +9,28 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactHomeAddressTests extends TestBase{
 
-    @Test
-    public void testHomeAddress() {
-
-        if (app.contact().all().size() == 0){
+    @BeforeMethod
+    public void ensurePreconditions() {
+        if (app.db().contacts().size() == 0) {
             String groupName = app.group().defineGroupName();
             app.goTo().backHome();
             app.contact().create(new ContactData().withName ("Mike").withLastName("Jordan")
                     .withMobilePhone("+33111222333").withEmail("terry.p@google.com").withHomeAddress("USA, Montana")
-                    .withGroup(groupName).withWorkPhone("121 5545").withHomePhone("444 5555"));
+                    .withGroup(groupName));
         }
+    }
+
+    @Test
+    public void testHomeAddress() {
 
         ContactData contact = app.contact().all().iterator().next();
         ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
 
         assertThat(contact.getHomeAddress(), equalTo(contactInfoFromEditForm.getHomeAddress()));
+
+        ContactData contact1 = app.db().contacts().iterator().next();
+        ContactData contactInfoFromEditForm1 = app.contact().infoFromEditForm(contact1);
+
+        assertThat(contact1.getHomeAddress(), equalTo(contactInfoFromEditForm1.getHomeAddress()));
     }
 }
