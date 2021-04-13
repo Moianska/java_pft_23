198f8143b0dab974d67c7e6e602c63af08bfa27d
Moianska <moyanskaya@gmail.com>
2021-04-14 00:32:15 +0300

    Modify ContactEmailTests with DB-loading data

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactEmailTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactEmailTests.java
@@ -1,7 +1,9 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
@@ -11,21 +13,31 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactEmailTests extends TestBase{
 
-    @Test
-    public void testEmail() {
-
-        if (app.contact().all().size() == 0){
+    @BeforeMethod
+    public void ensurePreconditions() {
+        if (app.db().contacts().size() == 0) {
             String groupName = app.group().defineGroupName();
             app.goTo().backHome();
             app.contact().create(new ContactData().withName ("Mike").withLastName("Jordan")
                     .withMobilePhone("+33111222333").withEmail("terry.p@google.com").withHomeAddress("USA, Montana")
-                    .withGroup(groupName).withWorkPhone("121 5545").withHomePhone("444 5555"));
+                    .withGroup(groupName));
         }
+    }
 
+    @Test
+    public void testEmail() {
+
+        app.goTo().backHome();
         ContactData contact = app.contact().all().iterator().next();
         ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
 
         assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
+
+        ContactData contact1 = app.db().contacts().iterator().next();
+        ContactData contactInfoFromEditForm1 = app.contact().infoFromEditForm(contact1);
+        assertThat(mergeEmails(contact1), equalTo(mergeEmails(contactInfoFromEditForm1)));
+
+
     }
 
     private String mergeEmails(ContactData contact) {
