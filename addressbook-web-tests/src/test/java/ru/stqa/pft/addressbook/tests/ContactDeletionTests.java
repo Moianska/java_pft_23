e289335077c02ca3295246558c84abdcfbd1d729
Moianska <moyanskaya@gmail.com>
2021-04-14 00:08:10 +0300

    Modify ContactDeletionTests with DB-loading data

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
@@ -10,22 +11,27 @@ import static org.testng.Assert.*;
 
 public class ContactDeletionTests extends TestBase{
 
+    @BeforeMethod
+    public void ensurePreconditions() {
+        if (app.db().contacts().size() == 0) {
+            String groupName = app.group().defineGroupName();
+            app.goTo().backHome();
+            app.contact().create(new ContactData().withName ("Mike").withLastName("Jordan")
+                    .withMobilePhone("+33111222333").withEmail("terry.p@google.com").withHomeAddress("USA, Montana")
+                    .withGroup(groupName));
+        }
+    }
+
     @Test
     public void testContactDeletion() {
 
         String groupName = app.group().defineGroupName();
         app.goTo().backHome();
 
-        if (app.contact().all().size() == 0){
-            app.contact().create(new ContactData().withName ("Mike").withLastName("Jordan")
-                    .withMobilePhone("+33111222333").withEmail("terry.p@google.com").withHomeAddress("USA, Montana")
-                    .withGroup(groupName));
-        }
-
-        Contacts before = app.contact().all();
+        Contacts before = app.db().contacts();
         ContactData deletedContact = before.iterator().next();
         app.contact().delete(deletedContact);
-        Contacts after = app.contact().all();
+        Contacts after = app.db().contacts();
 
         assertEquals(after.size(), before.size() - 1);
         assertThat(after, equalTo(before.without(deletedContact)));
