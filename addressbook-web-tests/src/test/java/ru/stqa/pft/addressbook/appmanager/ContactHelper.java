3c03387ccdf03bfd124263c0bc3f919ac60f543e
Moianska <moyanskaya@gmail.com>
2021-04-15 01:22:36 +0300

    Modifications after add link relations between Contact and Group

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.Groups;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,7 +30,11 @@ public class ContactHelper extends HelperBase {
         type(By.name("address"), contactData.getHomeAddress());
 
         if (creation) {
-            new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroup());
+            if (contactData.getGroups().size() > 0) {
+                Assert.assertTrue(contactData.getGroups().size() == 1);
+                new Select(wd.findElement(By.name("new_group")))
+                        .selectByVisibleText(contactData.getGroups().iterator().next().getGroupName());
+            }
             attach(By.name("photo"), contactData.getPhoto());
         } else {
             Assert.assertFalse(isElementPresent(By.name("new_group")));
@@ -72,7 +77,7 @@ public class ContactHelper extends HelperBase {
     }
 
     public void create(ContactData contact) {
-       initContactCreation();
+        initContactCreation();
         fillNewContactForm(contact, true);
         submitNewContactForm();
         backToHomePage();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -15,18 +16,18 @@ public class ContactModificationTests extends TestBase {
     @BeforeMethod
     public void ensurePreconditions() {
         if (app.db().contacts().size() == 0) {
-            String groupName = app.group().defineGroupName();
+            Groups groups = app.db().groups();
             app.goTo().backHome();
             app.contact().create(new ContactData().withName ("Mike").withLastName("Jordan")
                     .withMobilePhone("+33111222333").withEmail("terry.p@google.com").withHomeAddress("USA, Montana")
-                    .withGroup(groupName));
+                    .inGroup(groups.iterator().next()));
         }
     }
 
     @Test
     public void testContactModification() {
 
-        String groupName = app.group().defineGroupName();
+        Groups groups = app.db().groups();
         app.goTo().backHome();
 
         Contacts before = app.db().contacts();
@@ -34,7 +35,7 @@ public class ContactModificationTests extends TestBase {
         ContactData modifiedContact = before.iterator().next();
         ContactData finalContact = new ContactData().withId(modifiedContact.getId()).withName ("LLLL").withLastName("TTTT")
                 .withMobilePhone("+33111222333").withEmail("terry.p@google.com").withHomeAddress("USA, Montana")
-                .withGroup(groupName);
+                .inGroup(groups.iterator().next());
 
         app.contact().modify(finalContact);
 
