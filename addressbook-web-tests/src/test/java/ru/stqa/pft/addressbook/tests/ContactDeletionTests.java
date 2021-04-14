3d49661ccd4d402697e134c79c9f2846f7516fdc
Moianska <moyanskaya@gmail.com>
2021-04-15 01:17:01 +0300

    Modify ContactDeletionTests after add link relations between Contact and Group

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -4,6 +4,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.Groups;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -14,18 +15,17 @@ public class ContactDeletionTests extends TestBase{
     @BeforeMethod
     public void ensurePreconditions() {
         if (app.db().contacts().size() == 0) {
-            String groupName = app.group().defineGroupName();
+            Groups groups = app.db().groups();
             app.goTo().backHome();
             app.contact().create(new ContactData().withName ("Mike").withLastName("Jordan")
                     .withMobilePhone("+33111222333").withEmail("terry.p@google.com").withHomeAddress("USA, Montana")
-                    .withGroup(groupName));
+                    .inGroup(groups.iterator().next()));
         }
     }
 
     @Test
     public void testContactDeletion() {
 
-        String groupName = app.group().defineGroupName();
         app.goTo().backHome();
 
         Contacts before = app.db().contacts();
